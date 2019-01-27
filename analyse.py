3f7788d235da3509b4f5c49f88c78dfc7988eab6
braelynr <43896383+braelynr@users.noreply.github.com>
2019-01-27 06:26:26 -0500

    Python
    This analyzes a wav file. (download it from the sleeping slack)
    It correctly identifies the time durations of the episodes
    This runs on my linux virtual machine

---
+++ analyse.py
@@ -0,0 +1,23 @@
+from pydub import AudioSegment, silence
+
+myaudio = AudioSegment.from_wav("apnea_0_4.wav")
+
+silence = silence.detect_silence(myaudio, min_silence_len=1000, silence_thresh=-70)
+
+silence = [((start/1000),(stop/1000)) for start,stop in silence] #convert to sec
+
+apnea = 0
+realSilences = []
+episodes = 0
+
+for period in silence:
+	if (period[1] - period[0]) > 10:
+		realSilences.append(period)
+		episodes+=1
+
+print(realSilences)
+
+if len(realSilences) > 15: # current audio is like 5 minutes
+	apnea = 1
+
+print("hellome")
